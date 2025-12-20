ce29df5778bda9ec456f4585a3a5728ed9f6feee
vladislavkhamin90 <vladislavkhamin90@gmail.com>
2025-12-21 01:06:10 +0300

    Update WebSockets.kt

--- src/main/kotlin/com/example/plugin/WebSockets.kt
+++ src/main/kotlin/com/example/plugin/WebSockets.kt
@@ -2,14 +2,14 @@ package com.example.plugins
 
 import io.ktor.server.application.*
 import io.ktor.server.websocket.*
-import java.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureWebSockets() {
 
     install(WebSockets) {
-        pingPeriod = Duration.ofSeconds(15)
-        timeout = Duration.ofSeconds(30)
-        maxFrameSize = Long.MAX_VALUE
-        masking = false
+    pingPeriod = 15.seconds
+    timeout = 30.seconds
+    maxFrameSize = Long.MAX_VALUE
+    masking = false
     }
 }
