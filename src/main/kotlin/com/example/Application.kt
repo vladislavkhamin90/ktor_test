68e322b821853d97684a68375fdf34d2a63a0356
vladislavkhamin90 <vladislavkhamin90@gmail.com>
2025-12-21 01:15:09 +0300

    Update Application.kt

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,12 +1,23 @@
 package com.example
 
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 import com.example.plugins.*
 import com.example.routes.chatSocket
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
-fun Application.module() {
+fun main() {
+    embeddedServer(
+        Netty,
+        port = System.getenv("PORT")?.toInt() ?: 8080,
+        host = "0.0.0.0"
+    ) {
+        module()
+    }.start(wait = true)
+}
 
+fun Application.module() {
     configureSerialization()
     configureWebSockets()
 
